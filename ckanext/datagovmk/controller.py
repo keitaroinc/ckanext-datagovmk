faa42a1f19328db7dc770d3f9fb6130702150ef6
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-06-26 10:16:08 +0200

    Patch broken JS translations

--- ckanext/datagovmk/controller.py
+++ ckanext/datagovmk/controller.py
@@ -2,6 +2,9 @@
 """
 from ckan.controllers.package import PackageController
 from ckanext.datagovmk.model.stats import increment_downloads
+from ckan.lib.base import BaseController
+import os
+from ckan.plugins import toolkit
 
 class DownloadController(PackageController):
     """Overrides CKAN's PackageController to add count for the resource downloads.
@@ -13,3 +16,20 @@ class DownloadController(PackageController):
         """
         increment_downloads(resource_id)
         return super(DownloadController, self).resource_download(id, resource_id, filename)
+
+
+class ApiController(BaseController):
+    def i18n_js_translations(self, lang):
+        ''' Patch for broken JS translations caused by Pylons to Flask
+        migration. This method patches https://github.com/ckan/ckan/blob/master/ckan/views/api.py#L467 '''
+        ckan_path = os.path.join(
+            os.path.dirname(__file__), '..', '..', '..', 'ckan', 'ckan'
+        )
+        source = os.path.abspath(os.path.join(ckan_path, 'public',
+                                 'base', 'i18n', '%s.js' % lang))
+        toolkit.response.headers['Content-Type'] =\
+            'application/json;charset=utf-8'
+        if not os.path.exists(source):
+            return '{}'
+        f = open(source, 'r')
+        return(f)
