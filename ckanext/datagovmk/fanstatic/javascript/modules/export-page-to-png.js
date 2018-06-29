ed98e2c2aeaee88c48d943568a41b6a1b0ef7af6
Goce Mitevski <goce.mitevski@keitaro.com>
2018-06-29 18:52:13 +0200

    Move modules to javascript/modules

---
+++ ckanext/datagovmk/fanstatic/javascript/modules/export-page-to-png.js
@@ -0,0 +1,40 @@
+ckan.module('export-page-to-png', function($) {
+    return {
+        initialize: function() {
+            this.el.click(function(e){
+                e.preventDefault()
+                this.downloadMap()
+            }.bind(this))
+        },
+        downloadMap: function() {
+            var scrollY = window.pageYOffset;
+            var container = document.body;
+            var className = 'c3chart'
+            window.scrollTo(0, 0);
+            //fix weird back fill
+            d3.select('.' + className).selectAll("path").attr("fill", "none");
+            //fix no axes
+            d3.select('.' + className).selectAll("path.domain").attr("stroke", "black");
+            //fix no tick
+            d3.select('.' + className).selectAll(".tick line").attr("stroke", "black");
+
+            html2canvas(container, {
+              onrendered: function(canvas){
+                var a = document.createElement('a')
+                a.href = canvas.toDataURL()
+                a.download = 'chart.png'
+                document.body.appendChild(a)
+                a.click()
+                document.body.removeChild(a)
+              },
+                // allowTaint: true,
+                useCORS: true,
+                foreignObjectRendering: true,
+                ignoreElements: function(element) {
+                    return false
+                }
+            });
+            window.scrollTo(0, scrollY);
+        }
+    }
+})
