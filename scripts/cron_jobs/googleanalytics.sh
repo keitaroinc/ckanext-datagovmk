7799f53e534231f25bd18e981f6de9209918ce68
Aleksandar Jovanov <aleksandar.jovanov@keitaro.com>
2018-07-27 12:55:23 +0200

    Change credentials path

--- scripts/cron_jobs/googleanalytics.sh
+++ scripts/cron_jobs/googleanalytics.sh
@@ -1,4 +1,4 @@
 CONFIG_FILE_LOCATION=$1
 
 # Every hour
-paster --plugin=ckanext-googleanalytics loadanalytics credentials.json -c $CONFIG_FILE_LOCATION
+paster --plugin=ckanext-googleanalytics loadanalytics /var/lib/ckan/default/storage/ga_credentials.json -c $CONFIG_FILE_LOCATION
