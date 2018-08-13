f18494d0da54c2914953dcfffedf7e3de6e0f22f
Pavle Jonoski <pavle.jonoski@keitaro.com>
2018-08-13 15:58:18 +0200

    Added rewrite of stats functions.

---
+++ ckanext/datagovmk/stats.py
@@ -0,0 +1,200 @@
+import datetime
+
+from sqlalchemy import Table, select, join, func, and_
+
+from ckanext.stats.stats import Stats as CoreStats, RevisionStats as CoreRevisionStats, table, datetime2date, DATE_FORMAT
+
+import ckan.model as model
+import ckan.plugins as p
+from ckan.common import config
+
+cache_enabled = p.toolkit.asbool(config.get('ckanext.stats.cache_enabled', 'True'))
+cache_enabled = False
+
+if cache_enabled:
+    from pylons import cache
+    from ckanext.stats.stats import our_cache
+
+class Stats(CoreStats):
+    pass
+
+class RevisionStats(CoreRevisionStats):
+    
+    @classmethod
+    def get_num_packages_by_week(cls):
+        def num_packages():
+            new_packages_by_week = cls.get_by_week('new_packages')
+            deleted_packages_by_week = cls.get_by_week('deleted_packages')
+            first_date = (min(datetime.datetime.strptime(new_packages_by_week[0][0], DATE_FORMAT),
+                              datetime.datetime.strptime(deleted_packages_by_week[0][0], DATE_FORMAT))).date()
+            cls._cumulative_num_pkgs = 0
+            new_pkgs = []
+            deleted_pkgs = []
+            def build_weekly_stats(week_commences, new_pkg_ids, deleted_pkg_ids):
+                num_pkgs = len(new_pkg_ids) - len(deleted_pkg_ids)
+                new_pkgs.extend([model.Session.query(model.Package).get(id).name for id in new_pkg_ids])
+                deleted_pkgs.extend([model.Session.query(model.Package).get(id).name for id in deleted_pkg_ids])
+                cls._cumulative_num_pkgs += num_pkgs
+                return (week_commences.strftime(DATE_FORMAT),
+                        num_pkgs, cls._cumulative_num_pkgs)
+            week_ends = first_date
+            today = datetime.date.today()
+            new_package_week_index = 0
+            deleted_package_week_index = 0
+            weekly_numbers = [] # [(week_commences, num_packages, cumulative_num_pkgs])]
+            while week_ends <= today:
+                week_commences = week_ends
+                week_ends = week_commences + datetime.timedelta(days=7)
+                if datetime.datetime.strptime(new_packages_by_week[new_package_week_index][0], DATE_FORMAT).date() == week_commences:
+                    new_pkg_ids = new_packages_by_week[new_package_week_index][1]
+                    new_package_week_index += 1
+                else:
+                    new_pkg_ids = []
+                if datetime.datetime.strptime(deleted_packages_by_week[deleted_package_week_index][0], DATE_FORMAT).date() == week_commences:
+                    deleted_pkg_ids = deleted_packages_by_week[deleted_package_week_index][1]
+                    deleted_package_week_index += 1
+                    
+                else:
+                    deleted_pkg_ids = []
+                weekly_numbers.append(build_weekly_stats(week_commences, new_pkg_ids, deleted_pkg_ids))
+            # just check we got to the end of each count
+            assert new_package_week_index == len(new_packages_by_week)
+            assert deleted_package_week_index == len(deleted_packages_by_week)
+            return weekly_numbers
+        if cache_enabled:
+            week_commences = cls.get_date_week_started(datetime.date.today())
+            key = 'number_packages_%s' + week_commences.strftime(DATE_FORMAT)
+            num_packages = our_cache.get_value(key=key,
+                                               createfunc=num_packages)
+        else:
+            num_packages = num_packages()
+        return num_packages
+
+    @classmethod
+    def get_new_packages(cls):
+        '''
+        @return: Returns list of new pkgs and date when they were created, in
+                 format: [(id, date_ordinal), ...]
+        '''
+        def new_packages():
+            # Can't filter by time in select because 'min' function has to
+            # be 'for all time' else you get first revision in the time period.
+            package_revision = table('package_revision')
+            revision = table('revision')
+            s = select([package_revision.c.id, func.min(revision.c.timestamp)], from_obj=[package_revision.join(revision)]).\
+                where(package_revision.c.type == 'dataset').\
+                where(package_revision.c.state == 'active').\
+                group_by(package_revision.c.id).\
+                order_by(func.min(revision.c.timestamp))
+            res = model.Session.execute(s).fetchall() # [(id, datetime), ...]
+            res_pickleable = []
+            for pkg_id, created_datetime in res:
+                res_pickleable.append((pkg_id, created_datetime.toordinal()))
+            return res_pickleable
+        if cache_enabled:
+            week_commences = cls.get_date_week_started(datetime.date.today())
+            key = 'all_new_packages_%s' + week_commences.strftime(DATE_FORMAT)
+            new_packages = our_cache.get_value(key=key,
+                                               createfunc=new_packages)
+        else:
+            new_packages = new_packages()
+        return new_packages
+    
+    @classmethod
+    def get_by_week(cls, object_type):
+        cls._object_type = object_type
+        def objects_by_week():
+            if cls._object_type == 'new_packages':
+                objects = cls.get_new_packages()
+                def get_date(object_date):
+                    return datetime.date.fromordinal(object_date)
+            elif cls._object_type == 'deleted_packages':
+                objects = cls.get_deleted_packages()
+                def get_date(object_date):
+                    return datetime.date.fromordinal(object_date)
+            elif cls._object_type == 'package_revisions':
+                objects = cls.get_package_revisions()
+                def get_date(object_date):
+                    return datetime2date(object_date)
+            else:
+                raise NotImplementedError()
+            first_date = get_date(objects[0][1]) if objects else datetime.date.today()
+            week_commences = cls.get_date_week_started(first_date)
+            week_ends = week_commences + datetime.timedelta(days=7)
+            week_index = 0
+            weekly_pkg_ids = [] # [(week_commences, [pkg_id1, pkg_id2, ...])]
+            pkg_id_stack = []
+            cls._cumulative_num_pkgs = 0
+            def build_weekly_stats(week_commences, pkg_ids):
+                num_pkgs = len(pkg_ids)
+                cls._cumulative_num_pkgs += num_pkgs
+                return (week_commences.strftime(DATE_FORMAT),
+                        pkg_ids, num_pkgs, cls._cumulative_num_pkgs)
+            for pkg_id, date_field in objects:
+                date_ = get_date(date_field)
+                if date_ >= week_ends:
+                    weekly_pkg_ids.append(build_weekly_stats(week_commences, pkg_id_stack))
+                    pkg_id_stack = []
+                    week_commences = week_ends
+                    week_ends = week_commences + datetime.timedelta(days=7)
+                pkg_id_stack.append(pkg_id)
+            weekly_pkg_ids.append(build_weekly_stats(week_commences, pkg_id_stack))
+            today = datetime.date.today()
+            while week_ends <= today:
+                week_commences = week_ends
+                week_ends = week_commences + datetime.timedelta(days=7)
+                weekly_pkg_ids.append(build_weekly_stats(week_commences, []))
+            return weekly_pkg_ids
+        if cache_enabled:
+            week_commences = cls.get_date_week_started(datetime.date.today())
+            key = '%s_by_week_%s' % (cls._object_type, week_commences.strftime(DATE_FORMAT))
+            objects_by_week_ = our_cache.get_value(key=key,
+                                    createfunc=objects_by_week)
+        else:
+            objects_by_week_ = objects_by_week()
+        return objects_by_week_
+    
+    @classmethod
+    def get_deleted_packages(cls):
+        '''
+        @return: Returns list of deleted pkgs and date when they were deleted, in
+                 format: [(id, date_ordinal), ...]
+        '''
+        def deleted_packages():
+            # Can't filter by time in select because 'min' function has to
+            # be 'for all time' else you get first revision in the time period.
+            package_revision = table('package_revision')
+            revision = table('revision')
+            s = select([package_revision.c.id, func.min(revision.c.timestamp)], from_obj=[package_revision.join(revision)]).\
+                where(package_revision.c.state==model.State.DELETED).\
+                 where( package_revision.c.type == 'dataset').\
+                group_by(package_revision.c.id).\
+                order_by(func.min(revision.c.timestamp))
+            res = model.Session.execute(s).fetchall() # [(id, datetime), ...]
+            res_pickleable = []
+            for pkg_id, deleted_datetime in res:
+                res_pickleable.append((pkg_id, deleted_datetime.toordinal()))
+            return res_pickleable
+        if cache_enabled:
+            week_commences = cls.get_date_week_started(datetime.date.today())
+            key = 'all_deleted_packages_%s' + week_commences.strftime(DATE_FORMAT)
+            deleted_packages = our_cache.get_value(key=key,
+                                                   createfunc=deleted_packages)
+        else:
+            deleted_packages = deleted_packages()
+        return deleted_packages
+
+    @classmethod
+    def get_package_revisions(cls):
+        '''
+        @return: Returns list of revisions and date of them, in
+                 format: [(id, date), ...]
+        '''
+        package_revision = table('package_revision')
+        revision = table('revision')
+        s = select([package_revision.c.id, revision.c.timestamp], from_obj=[package_revision.join(revision)]).\
+            where(package_revision.c.type == 'dataset').\
+            where(package_revision.c.state == 'active').\
+            order_by(revision.c.timestamp)
+        res = model.Session.execute(s).fetchall() # [(id, datetime), ...]
+        return res
