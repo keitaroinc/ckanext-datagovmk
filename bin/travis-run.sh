54daf3f1e417b95c5b449756284137b69adb4691
Vladimir Tomanovski <vladoohr@gmail.com>
2018-11-13 12:11:05 +0100

    Start SMTP server in background dorectly

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -3,7 +3,7 @@
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
-sudo python -m smtpd -n -c DebuggingServer localhost:6675
+sudo python -m smtpd -n -c DebuggingServer localhost:6675 &
 
 nosetests --ckan \
           --nologcapture \
